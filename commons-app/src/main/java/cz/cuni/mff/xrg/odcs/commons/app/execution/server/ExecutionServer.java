c3b94f5a3de9e869c7093554d20c0d9f7acfc3ee
Tomas Knap <toknap@gmail.com>
2017-05-31 14:50:27 +0200

    Fixed wrong annotation causing issues with pgsql

--- commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/execution/server/ExecutionServer.java
+++ commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/execution/server/ExecutionServer.java
@@ -21,10 +21,10 @@ public class ExecutionServer implements Serializable, DataObject {
     @Column(name = "id")
     private Long id;
 
-    // TIMESTAMP won't work with MS SQL Server in this case, hence
-    // changed to DATETIME.
+    // TIMESTAMP won't work with MS SQL Server in this case, hence has to be
+    // changed to DATETIME (columnDefinition = "DATETIME")
     @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "last_update", nullable = false, columnDefinition = "DATETIME")
+    @Column(name = "last_update", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private Date lastUpdate;
 
     @Column(name = "backend_id", unique = true, length = 128)
